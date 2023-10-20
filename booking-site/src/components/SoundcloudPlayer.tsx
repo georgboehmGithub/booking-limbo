53c3f7912ec74be35a776771cbe5541a96e2552f
georgboehmGithub <georgboehmcc@hotmail.com>
2023-10-20 09:35:58 +0200

    hide email credentials in env variable for making repo public

--- booking-site/src/components/SoundcloudPlayer.tsx
+++ booking-site/src/components/SoundcloudPlayer.tsx
@@ -11,7 +11,7 @@ const SoundcloudPlayer: React.FC<Props> = ({ linkStyles, divStyle }) => (
       height="166"
       allow="autoplay"
       src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1603444233&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
-    ></iframe>
+    />
     <div style={divStyle}>
       <a
         href="https://soundcloud.com/limbo_m"
