40129689469872518e16b06f10f9f5cd5404a4d5
georgboehmGithub <georgboehmcc@hotmail.com>
2023-10-20 09:38:44 +0200

    more refactorings

--- backend-services/app.js
+++ backend-services/app.js
@@ -1,16 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 8000; // Set your desired port
-const cors = require('cors');
-require('dotenv').config();
+const port = 8000;
+const cors = require("cors");
+require("dotenv").config();
 app.use(express.json());
-const nodemailer = require('nodemailer');
-// Enable CORS for all routes
-app.use(cors({origin: process.env.ENVIRONMENT === "LOCAL" ? "http://localhost:3000" : [process.env.API_URL, process.env.API_URL_WWW]}));
+const nodemailer = require("nodemailer");
 
+// Enable CORS for particular routes only
+app.use(
+  cors({
+    origin:
+      process.env.ENVIRONMENT === "LOCAL"
+        ? "http://localhost:3000"
+        : [process.env.API_URL, process.env.API_URL_WWW],
+  })
+);
 
 // Create a transporter using Gmail SMTP
-  // TODO: Environment variables for email credentials
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
@@ -22,14 +28,14 @@ const transporter = nodemailer.createTransport({
 });
 
 // Define a sample route
-app.post('/api/send-email', cors(), (req, res) => {
-    const { name, phoneNumber, date, time, message } = req.body;
+app.post("/api/send-email", cors(), (req, res) => {
+  const { name, phoneNumber, date, time, message } = req.body;
 
   // Email content
   const mailOptions = {
     from: process.env.BOOKING_REQUEST_SENDER_EMAIL,
-    to: 'georgboehm@outlook.com', // Replace with your recipient's email address
-    subject: 'New Booking Request',
+    to: process.env.BOOKING_RECIPIENT_EMAIL,
+    subject: "New Booking Request",
     text: `
       Name: ${name}
       Phone number: ${phoneNumber}
@@ -39,17 +45,12 @@ app.post('/api/send-email', cors(), (req, res) => {
     `,
   };
 
-	console.log("mail options: ", mailOptions)
-	console.log("transporter: ", transporter)
   // Send the email
   transporter.sendMail(mailOptions, (error, info) => {
-	console.log("sending email now!")
     if (error) {
-      console.error('Error sending email:', error);
-      res.status(500).json({ message: 'Error sending email' });
+      res.status(500).json({ message: "Error sending email" });
     } else {
-      console.log('Email sent:', info.response);
-      res.json({ message: 'Email sent successfully' });
+      res.json({ message: "Email sent successfully" });
     }
   });
 });
